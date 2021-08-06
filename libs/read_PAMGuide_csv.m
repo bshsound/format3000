0c5cf592e8f5a459f59e541b23e51f72868138de
bshsound <62338904+bshsound@users.noreply.github.com>
2021-08-06 10:45:31 +0000

    Update
    Added: Import of PAMGuide .csv files
    Corrected: Import to Matlab Workspace

---
+++ libs/read_PAMGuide_csv.m
@@ -0,0 +1,10 @@
+%% function, that loads PAMGuide's output .csv files, 
+% extracts the data and converts it to the right data format to include it 
+% in the ICES's .hdf5 format
+% filename = 'X:\Meereskunde\Unterwasserschall\BSH-Interreg-JOMOPANS\70_Daten\10_Prozessierung\SCO\Arbroath 10 - 01JAN2019 - 07FEB2019.csv';
+function [t,f,f_count,ch_count,TOL] = read_PAMGuide_csv(filename)
+%%
+
+%%
+
+end
